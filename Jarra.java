7ac7a1aa0e969f357d8e6199d8f4eb192f136a0d
Jose Lozano <timoningeniero@gmail.com>
2016-03-15 18:37:25 +0100

    holis

--- Jarra.java
+++ Jarra.java
@@ -11,4 +11,12 @@ public class Jarra {
 			}
 			this.capacidad=capacidadInicial;
 		}
+	public void llenaDesde (Jarra j){
+	while ((this.cantidad<this.capacidad)&&(j.cantidad>0)){
+	this.cantidad++;
+	j.cantidad--;
+	}
+	public String toString(){
+	return ("("+this.capacidad+","+this.cantidad+")");
+	}
 }
